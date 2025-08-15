19c2f76b43e75d58b7cef49c59b8e6fed5d58e2f
unknown <cbkjh0225@gmail.com>
2025-08-15 20:30:36 +0900

    first

---
+++ src/test/java/com/example/Campung/CampungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Campung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
