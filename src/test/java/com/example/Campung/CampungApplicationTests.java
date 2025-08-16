bb9d75281639fd46783b80ada4c64c1db30b4d14
unknown <cbkjh0225@gmail.com>
2025-08-16 16:32:26 +0900

    swagger added

--- src/test/java/com/example/Campung/CampungApplicationTests.java
+++ src/test/java/com/example/Campung/CampungApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.Campung;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // test 프로파일 활성화 - H2 DB 사용, Redis 비활성화
 class CampungApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Spring Context가 정상적으로 로드되는지 테스트
+		// H2 인메모리 DB 사용, Redis는 비활성화
 	}
 
 }
