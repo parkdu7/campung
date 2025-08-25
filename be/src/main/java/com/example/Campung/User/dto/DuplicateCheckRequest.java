e46bc89631d25fec723f1b343cf8029715f914c8
parkdu7 <tmdrbs0925@gmail.com>
2025-08-25 22:47:12 +0900

    delete : be 폴더 삭제!!!

--- be/src/main/java/com/example/Campung/User/dto/DuplicateCheckRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.Campung.User.Dto;
-
-public class DuplicateCheckRequest {
-    private String userId;
-
-    public String getUserId() { return userId; }
-    public void setUserId(String userId) { this.userId = userId; }
-}
