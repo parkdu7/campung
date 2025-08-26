3b07744edb84a698889311500012fbe740706284
이상헌 <rntehr2@naver.com>
2025-08-26 22:46:49 +0900

    feat : NotificationApi,NotificationListResponse, NotificationResponse, NotificationRepository,NotificationScreen, NotificationViewModel, LocationRepository, LocationApi fix : MainActivity, LocationShareRespond, AuthRepository, NetworkModule, Route, HomeScreen 알림 기능 구현

--- android/campung/app/src/main/java/com/shinhan/campung/MainActivity.kt
+++ android/campung/app/src/main/java/com/shinhan/campung/MainActivity.kt
@@ -33,6 +33,7 @@ import com.shinhan.campung.presentation.ui.screens.FriendScreen
 import com.shinhan.campung.presentation.ui.screens.FullMapScreen
 import com.shinhan.campung.presentation.ui.screens.HomeScreen
 import com.shinhan.campung.presentation.ui.screens.LoginScreen
+import com.shinhan.campung.presentation.ui.screens.NotificationScreen
 import com.shinhan.campung.presentation.ui.screens.SignupScreen
 import com.shinhan.campung.presentation.ui.theme.CampungTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -169,6 +170,13 @@ private fun AppNav(authDataStore: AuthDataStore) {
             }
         ) {
             FullMapScreen(navController = navController)
+        }
+
+        // 알림 화면
+        composable(Route.NOTIFICATION) {
+            NotificationScreen(
+                onBackClick = { navController.popBackStack() }
+            )
         }
     }
 }
--- android/campung/app/src/main/java/com/shinhan/campung/data/remote/request/LocationShareRespondRequest.kt
+++ android/campung/app/src/main/java/com/shinhan/campung/data/remote/request/LocationShareRespondRequest.kt
@@ -4,6 +4,9 @@ import com.google.gson.annotations.SerializedName
 import java.math.BigDecimal
 
 data class LocationShareRespondRequest(
+    @SerializedName("locationRequestId")
+    val locationRequestId: Long? = null,
+    
     @SerializedName("action")
     val action: String, // "accept" or "reject"
     
